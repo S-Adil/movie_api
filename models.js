e0e1b9cc6b2335193a5a306c140539da4ba7e9e0
S-Adil <sana.adil.ak@gmail.com>
2022-04-13 17:48:23 -0400

    Update models.js
    added bcrypt model for password hashing

--- models.js
+++ models.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
 let movieSchema = mongoose.Schema({
   Title: {type: String, required: true},
   Description: {type: String, required: true},
@@ -20,8 +22,16 @@ let userSchema = mongoose.Schema({
   Password: {type: String, required: true},
   Email: {type: String, required: true},
   Birthday: Date,
-  FavouriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref:'movie' }]
+  FavouriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref:'Movie' }]
 });
+
+userSchema.statics.hashPassword = (password) => {
+  return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compareSync(password, this.Password);
+};
 
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
