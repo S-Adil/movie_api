83a89a2dc77a23b8d717e7c3164f5d2ec6998542
S-Adil <sana.adil.ak@gmail.com>
2022-04-13 17:50:24 -0400

    added password validation for hashed user password

--- passport.js
+++ passport.js
@@ -39,3 +39,27 @@ const passport = require('passport'),
         return callback(error)
       });
     }));
+
+    passport.use(new LocalStrategy({
+      usernameField: 'Username',
+      passwordField: 'Password'
+    }, (username, password, callback) => {
+      console.log(username + '  ' + password);
+      Users.findOne({ Username: username }, (error, user) => {
+        if (error) {
+          console.log(error);
+          return callback(error);
+        }
+
+        if (!user) {
+          console.log('incorrect username');
+          return callback(null, false, {message: 'Incorrect username.' });
+        }
+        if (!user.validatePassword(password)) {
+          console.log('incorrect password');
+          return callback(null, false, {message: 'Incorrect password.'});
+        }
+        console.log('finished');
+        return callback(null, user);
+      });
+    }));
